292245c25729dd609eb5f3e06b2c79c69f3ba924
Ioav Mizrachi <ioavmizra22@gmail.com>
2026-05-11 13:25:37 -0500

    cambio de formato

--- src/assets/imagenes.ts
+++ src/assets/imagenes.ts
@@ -1,6 +1,5 @@
 export { default as automatizacion } from './automatizacion.webp'
 export { default as iluminacion } from './iluminacion.webp'
-export { default as seguridad } from './seguridad.webp'
 export { default as casa } from './casa.webp'
 export { default as CasaModerna } from './casa-moderna.webp'
 export { default as casa2 } from './casa2.webp'
